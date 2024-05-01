0982121357b2861dc80b61fd4708dfe897758740
vinay-works <vinayofficial937@gmail.com>
2024-05-01 16:20:23 +0530

    modified error

--- src/components/main-body/index.js
+++ src/components/main-body/index.js
@@ -24,6 +24,7 @@ const Indexpage = () => {
       setDocsId(match[1]);
     } else {
       setDocsId("");
+      toast.error("Please Provice Public Google Docs Url");
     }
   };
 
@@ -40,6 +41,7 @@ const Indexpage = () => {
       return;
     }
 
+  
     setIsSubmitted(true); // Set isSubmitted to true when the form is submitted
     setIsConverting(true); // Start conversion process
     setLoading(true); // Set loading to true before starting the conversion
@@ -52,8 +54,16 @@ const Indexpage = () => {
   };
 
   const convertToMarkdown = async (docsId) => {
+    
     try {
-      const response = await axios.get(`/api/markdown-backend?docsId=${docsId}`);
+      const response = await axios.get(
+        `/api/markdown-backend?docsId=${docsId}`
+      );
+
+      if (response.data.markdown == null) {
+        toast.error("Please Paste a valid public Google Docs link.");
+        return;
+      }
       console.log(response.data.markdown);
       setMarkdown(response.data.markdown);
       toast.success("Conversion successful!");
@@ -62,8 +72,8 @@ const Indexpage = () => {
       setError(error.message);
       toast.error("Conversion failed. Please try again.");
     } finally {
-      setLoading(false); // Set loading to false after the conversion is complete
-      setIsConverting(false); // Stop conversion process
+      setLoading(false); 
+      setIsConverting(false);
     }
   };
 
@@ -73,7 +83,7 @@ const Indexpage = () => {
     setMarkdown("");
     setError("");
     setIsSubmitted(false);
-    setIsConverting(false); 
+    setIsConverting(false);
   };
 
   return (
@@ -106,55 +116,54 @@ const Indexpage = () => {
               with the link can view.
             </div>
             <div className="flex justify-center">
-          {isSubmitted? (
-            isConverting? (
-              <div className="flex items-center justify-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-purple-500 focus:outline-none focus:bg-blue-700">
-                <svg
-                        aria-hidden="true"
-                        role="status"
-                        className="inline w-4 h-4 me-3 text-gray-200 animate-spin dark:text-gray-600"
-                        viewBox="0 0 100 100"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                          fill="currentColor"
-                        />
-                        <path
-                          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                          fill="#1C64F2"
-                        />
-                      </svg>
-                Processing...
-              </div>
-            ) : (
-              <button
-                type="button"
-                onClick={handleReset}
-                className="flex items-center justify-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-purple-500 focus:outline-none focus:bg-blue-700"
-              >
-                Reset
-              </button>
-            )
-          ) : (
-            <button
-              type="submit"
-              className="flex items-center justify-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-purple-500 focus:outline-none focus:bg-blue-700"
-              disabled={loading}
-            >
-              {loading? (
-                <div className="flex items-center">
-                </div>
+              {isSubmitted ? (
+                isConverting ? (
+                  <div className="flex items-center justify-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-purple-500 focus:outline-none focus:bg-blue-700">
+                    <svg
+                      aria-hidden="true"
+                      role="status"
+                      className="inline w-4 h-4 me-3 text-gray-200 animate-spin dark:text-gray-600"
+                      viewBox="0 0 100 100"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="#1C64F2"
+                      />
+                    </svg>
+                    Processing...
+                  </div>
+                ) : (
+                  <button
+                    type="button"
+                    onClick={handleReset}
+                    className="flex items-center justify-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-purple-500 focus:outline-none focus:bg-blue-700"
+                  >
+                    Reset
+                  </button>
+                )
               ) : (
-                "Convert To Markdown"
+                <button
+                  type="submit"
+                  className="flex items-center justify-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-purple-500 focus:outline-none focus:bg-blue-700"
+                  disabled={loading}
+                >
+                  {loading ? (
+                    <div className="flex items-center"></div>
+                  ) : (
+                    "Convert To Markdown"
+                  )}
+                  {!loading && <FaArrowRightLong className="ml-2" />}
+                </button>
               )}
-              {!loading && <FaArrowRightLong className="ml-2" />}
-            </button>
-          )}
-          {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
-        </div>
-      </form>
+              {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
+            </div>
+          </form>
         </div>
       </div>
 
@@ -166,15 +175,13 @@ const Indexpage = () => {
                 <h3 className="text-center text-black font-bold text-xl">
                   Converted Markdown
                 </h3>
-                
               </div>
               <div className="flex justify-end items-center mb-4">
                 <button
                   onClick={copyToClipboard}
                   className="flex justify-center items-center gap-2 bg-blue-600 text-white p-2 rounded hover:bg-purple-500 focus:outline-none ml-2"
                 >
-                    Copy
-                
+                  Copy
                   <FaCopy />
                 </button>
               </div>
