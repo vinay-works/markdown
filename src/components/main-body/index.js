e5f98549f02914c388e4b116e23d0c9b19c2c24f
vinay-works <vinayofficial937@gmail.com>
2024-05-05 14:14:33 +0530

    modified ui

--- src/components/main-body/index.js
+++ src/components/main-body/index.js
@@ -10,7 +10,7 @@ const Indexpage = () => {
   const [docsId, setDocsId] = useState("");
   const [markdown, setMarkdown] = useState("");
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const [isConverting, setIsConverting] = useState(false); 
+  const [isConverting, setIsConverting] = useState(false);
   const handleInputChange = (event) => {
     const inputUrl = event.target.value;
     setUrl(inputUrl);
@@ -39,11 +39,10 @@ const Indexpage = () => {
       return;
     }
 
-  
-    setIsSubmitted(true); 
+    setIsSubmitted(true);
     setIsConverting(true);
-    setLoading(true); 
-    convertToMarkdown(docsId); 
+    setLoading(true);
+    convertToMarkdown(docsId);
   };
 
   const copyToClipboard = () => {
@@ -52,7 +51,6 @@ const Indexpage = () => {
   };
 
   const convertToMarkdown = async (docsId) => {
-    
     try {
       const res = await fetch(`/api/markdown-backend?docsId=${docsId}`, {
         method: "GET",
@@ -63,7 +61,7 @@ const Indexpage = () => {
         return;
       }
       const data = await res.json();
-      
+
       if (!data.markdown) {
         toast.error("Please Paste a valid public Google Docs link.");
         return;
@@ -76,7 +74,7 @@ const Indexpage = () => {
       setError(error.message);
       toast.error("Conversion failed. Please try again.");
     } finally {
-      setLoading(false); 
+      setLoading(false);
       setIsConverting(false);
     }
   };
@@ -194,8 +192,86 @@ const Indexpage = () => {
         )}
       </div>
 
+      <div className=" h-30  rounded-lg text-black flex flex-col justify-center items-center mt-12">
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4 px-4 md:px-8 lg:px-16 xl:px-32">
+          <div class="p-8 border border-gray-200 rounded-lg">
+            <div class="bg-yellow-100 rounded-full w-16 h-16 flex justify-center items-center text-orange-500  mx-auto">
+              {/* <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="w-11 h-11 hover:animate-pulse"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12 2.25a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V3a.75.75 0 0 1 .75-.75Zm-9 13.5a.75.75 0 0 1 .75.75v2.25a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V16.5a.75.75 0 0 1 1.5 0v2.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V16.5a.75.75 0 0 1 .75-.75Z"
+                  clip-rule="evenodd"
+                />
+              </svg> */}
+              <svg class="w-[48px] h-[48px]  dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linejoin="round" stroke-width="0.8" d="M9 8v3a1 1 0 0 1-1 1H5m11 4h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-7a1 1 0 0 0-1 1v1m4 3v10a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-7.13a1 1 0 0 1 .24-.65L7.7 8.35A1 1 0 0 1 8.46 8H13a1 1 0 0 1 1 1Z"/>
+</svg>
 
-     
+            </div>
+            <h2 class="uppercase mt-6 text-black font-medium text-center mb-3">
+              Easy Copy Markdown
+            </h2>
+            <p class="font-light text-sm text-gray-500 mb-3 text-center">
+              Simplify Markdown copying for quick and easy integration.
+            </p>
+          </div>
+
+          <div class="p-8 border border-gray-200 rounded-lg">
+            <div class="bg-green-100 rounded-full w-16 h-16 flex justify-center items-center text-green-500  mx-auto">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="w-11 h-11 hover:animate-pulse"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12.516 2.17a.75.75 0 0 0-1.032 0 11.209 11.209 0 0 1-7.877 3.08.75.75 0 0 0-.722.515A12.74 12.74 0 0 0 2.25 9.75c0 5.942 4.064 10.933 9.563 12.348a.749.749 0 0 0 .374 0c5.499-1.415 9.563-6.406 9.563-12.348 0-1.39-.223-2.73-.635-3.985a.75.75 0 0 0-.722-.516l-.143.001c-2.996 0-5.717-1.17-7.734-3.08Zm3.094 8.016a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+            <h2 class="uppercase mt-6 text-black font-medium text-center mb-3">
+              Privacy Assured
+            </h2>
+            <p class="font-light text-sm text-gray-500 mb-3 text-center">
+              Your data remains secure; we only require the Google Docs URL for
+              extraction.
+            </p>
+          </div>
+
+          <div class="p-8 border border-gray-200 rounded-lg">
+            <div class="bg-indigo-100 rounded-full w-16 h-16 flex justify-center items-center text-indigo-500  mx-auto ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="w-11 h-11 hover:animate-pulse"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M9 4.5a.75.75 0 0 1 .721.544l.813 2.846a3.75 3.75 0 0 0 2.576 2.576l2.846.813a.75.75 0 0 1 0 1.442l-2.846.813a3.75 3.75 0 0 0-2.576 2.576l-.813 2.846a.75.75 0 0 1-1.442 0l-.813-2.846a3.75 3.75 0 0 0-2.576-2.576l-2.846-.813a.75.75 0 0 1 0-1.442l2.846-.813A3.75 3.75 0 0 0 7.466 7.89l.813-2.846A.75.75 0 0 1 9 4.5ZM18 1.5a.75.75 0 0 1 .728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 0 1 0 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 0 1-1.456 0l-.258-1.036a2.625 2.625 0 0 0-1.91-1.91l-1.036-.258a.75.75 0 0 1 0-1.456l1.036-.258a2.625 2.625 0 0 0 1.91-1.91l.258-1.036A.75.75 0 0 1 18 1.5ZM16.5 15a.75.75 0 0 1 .712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 0 1 0 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 0 1-1.422 0l-.395-1.183a1.5 1.5 0 0 0-.948-.948l-1.183-.395a.75.75 0 0 1 0-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0 1 16.5 15Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+            <h2 class="uppercase mt-6 text-black font-medium text-center mb-3">
+              Markdown! Everywhere
+            </h2>
+            <p class="font-light text-sm text-gray-500 mb-3 text-center">
+              Markdown versatility extends to blogging, messaging, forums,
+              collaboration, documentation, and more.
+            </p>
+
+          </div>
+        </div>
+      </div>
+
       {/* last section */}
       <div className="pb-8 mt-12">
         <div className="text-black font-bold text-lg flex flex-col lg:flex-row justify-center items-center mt-8">
